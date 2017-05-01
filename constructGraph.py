35cb73704f0d0d80a105ef091980c58cbcadbb18
Pkalra7 <pritish_kalra@yahoo.co.in>
2017-05-01 16:07:43 -0400

    Adding Execution Files
    These files construct the graph and solve the puzzles. Main is also
    added.

---
+++ constructGraph.py
@@ -0,0 +1,147 @@
+from easyThree import sudokuInstance
+
+labeledNodes={}
+
+def buildgraph(sudokuInstance):
+
+        rowVert=[]
+        columnVert=[]
+        subgridVert=[]
+        
+        rowVert=[i for i in range (1,10)]
+
+        columnVert=[(9*i + 1) for i in range(0,9)]
+
+        subgridVert=[1,2,3,10,11,12,19,20,21]
+        
+        bigGrid=[]
+        
+        bigGrid.append(subgridVert)
+        
+        firstCell=[4,7,28,31,34,55,58,61]
+        
+        for cell in firstCell:
+                incrementBlock=cell-1
+                eachSubGrid=[x+incrementBlock for x in subgridVert]
+                bigGrid.append(eachSubGrid)
+        
+
+        sudokuGraph={}
+        
+        for vertex in range(1,82):
+                sudokuGraph[vertex]=[]
+
+        sudokuGraph=addRowEdges(sudokuGraph,rowVert)
+        sudokuGraph=addColumnEdges(sudokuGraph,columnVert)
+        sudokuGraph=addGridEdges(sudokuGraph,bigGrid)
+        
+       
+
+                
+        
+        sudokuGraph=initialColouring(sudokuInstance,sudokuGraph)
+
+        
+        return sudokuGraph
+        
+def addRowEdges(graph,rowVert):
+
+        r=0
+        for cell in range(1,82):
+                rEdges=[]
+                rEdges=[9*r+x for x in rowVert]
+                if cell in rEdges:
+                        for num in rEdges:
+                                if num!=cell:
+                                        if num not in graph[cell]:
+                                                graph[cell].append(num)
+                                                if cell not in graph[num]:
+                                                        graph[num].append(cell)
+                if cell%9==0:
+                        r+=1
+
+        return graph
+
+def addColumnEdges(graph,column):
+        for cell in range(1,82):
+                rEdges=column[:]
+                if cell in rEdges:
+                        for num in rEdges:
+                                if num!=cell:
+                                        if num not in graph[cell]:
+                                                graph[cell].append(num)
+                                                if cell not in graph[num]:
+                                                        graph[num].append(cell)
+
+                else:
+                        
+                        diff=cell-1
+                        
+                        index=0
+                        
+                        for j in range(0,len(rEdges)):
+                                if rEdges[j]+diff<=81:
+                                        rEdges[j]=rEdges[j]+diff
+                                else:
+                                        break
+                                index=index+1
+                
+                        li=rEdges[:index]
+
+                        if cell in li:
+                                for num in li:
+                                        if num!=cell:
+                                                if num not in graph[cell]:
+                                                        graph[cell].append(num)
+                                                        if cell not in graph[num]:
+                                                                graph[num].append(cell)
+        
+        
+        return graph
+
+
+def addGridEdges(graph,bGrid):
+        for cell in range(1,82):
+                for grid in bGrid:
+                        if cell in grid:
+                                rEdges=grid
+                                for num in rEdges:
+                                        if num!=cell:
+                                                if num not in graph[cell]:
+                                                        graph[cell].append(num)
+                                                        if cell not in graph[num]:
+                                                                graph[num].append(cell)
+        return graph
+
+
+
+
+def initialColouring(sudokuInstance,graph):
+        #use global variable to use in other files
+        global labeledNodes
+        
+        for i in range(1,10):
+                for j in range(1,10):
+                        if sudokuInstance[i][j]!=0:
+                                labeledNodes[9*(i-1)+j]=sudokuInstance[i][j]
+        
+        for this in labeledNodes:
+                for other in labeledNodes:
+                        if this!=other and labeledNodes[this]!=labeledNodes[other]:
+                                if other not in graph[this]:
+                                        graph[this].append(other)
+                                        if this not in graph[other]:
+                                                graph[other].append(this)
+        
+        for thisOne in labeledNodes:
+                for otherOne in labeledNodes:
+                        if thisOne!=otherOne and labeledNodes[thisOne]==labeledNodes[otherOne]:
+                                for node in graph[otherOne]:
+                                        if node not in graph[thisOne] and node!=thisOne:
+                                                graph[thisOne].append(node)
+     
+        
+                               
+        return graph
+        
+
